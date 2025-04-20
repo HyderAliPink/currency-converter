64c264f81f984afcab69946bbea30eca88c82ad5
Rayyan Asrar <145535106+RayyanAsrar@users.noreply.github.com>
2025-04-20 18:26:32 +0500

    cleanedCode

--- app.js
+++ app.js
@@ -1,33 +1,19 @@
-// const url = `https://api.exchangerate.host/convert?from=${from}&to=${to}&amount=${result}`;
-const url = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.json`;
-fetch(url)
-  .then((Response) => {
-    if (!Response.ok) {
-      console.log(Response);
-      throw new Error("respone was not ok");
+let selectOne= document.getElementById('fromCurrency')
+let selectTwo= document.getElementById('toCurrency')
+  let getData = async ()=>{
+    try{
+    const res= await axios("https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.json")
+    return res
     }
-    return Response.json();
-  })
-  .then((Response) => {
-    console.log(Response);
-    const CurrencyList = Response;
-    for (const code in CurrencyList) {
-      console.log(code);
-      const CurrencyList = code;
-      const option = document.createElement("option");
-      option.value = code;
-      option.textContent = `${code}`;
-      fromCurrency.appendChild(option);
-    }
-    for (const code in CurrencyList) {
-      console.log(code);
-      const currecy = CurrencyList[code];
-      const option = document.createElement("option");
-      option.value = code;
-      option.textContent = `${code}`;
-      toCurrency.appendChild(option);
-    }
-  })
-  .catch((Error) => {
-    console.error(Error);
-  });
+    catch(err){
+        throw err;
+} 
+}
+getData().then((res)=>{
+  currency=res.data
+  for (const key in currency) {
+    selectOne.innerHTML+=`<option value="${key}">${currency[key]}</option>`
+    selectTwo.innerHTML+=`<option value="${key}">${currency[key]}</option>`
+    
+  }
+})
