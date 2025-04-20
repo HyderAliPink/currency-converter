6b534eb12d77ca2013bf9be5b8f46eb7b6397fb7
Rayyan Asrar <145535106+RayyanAsrar@users.noreply.github.com>
2025-04-20 20:46:01 +0500

    completed

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 let selectOne= document.getElementById('fromCurrency')
 let selectTwo= document.getElementById('toCurrency')
+let resultBox= document.getElementById('resultBox')
+let userInput= document.getElementById('input-1')
+
   let getData = async ()=>{
     try{
     const res= await axios("https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies.json")
@@ -9,11 +12,30 @@ let selectTwo= document.getElementById('toCurrency')
         throw err;
 } 
 }
+
 getData().then((res)=>{
-  currency=res.data
+  let currency=res.data
   for (const key in currency) {
     selectOne.innerHTML+=`<option value="${key}">${currency[key]}</option>`
     selectTwo.innerHTML+=`<option value="${key}">${currency[key]}</option>`
-    
+  
   }
 })
+
+function exchange(){
+  let fromCurrency= selectOne.value
+  let toCurrency= selectTwo.value
+  if (userInput.value === "" || userInput.value<0) {
+    resultBox.innerText = "Please enter a valid amount amount.";
+    
+  }else{
+    axios(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${fromCurrency}.json`)
+    .then((res)=>{
+     let rate = res.data[fromCurrency][toCurrency]
+  let result = userInput.value*rate
+  resultBox.innerText = `${userInput.value} ${fromCurrency.toUpperCase()} = ${result.toFixed(2)} ${toCurrency.toUpperCase()}`
+    })
+  }
+  }
+  
+  
